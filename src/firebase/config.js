15860305503049747ba3db340aa5f52c09376b60
HassanKhan123 <hk2602809@gmail.com>
2020-07-30 23:02:09 +0500

    firebase setup initialized

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+var firebaseConfig = {
+  apiKey: "AIzaSyBA8j0M3zVjZGN_p5tKcC5pxCy1WChXz7M",
+  authDomain: "firegram-react-firebase.firebaseapp.com",
+  databaseURL: "https://firegram-react-firebase.firebaseio.com",
+  projectId: "firegram-react-firebase",
+  storageBucket: "firegram-react-firebase.appspot.com",
+  messagingSenderId: "74618391907",
+  appId: "1:74618391907:web:31639c7c707974b7791756",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
