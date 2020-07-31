18c5f90b77f35263902e8e6e57bb2ff3a92c3128
HassanKhan123 <hk2602809@gmail.com>
2020-07-31 13:52:51 +0500

    Storage hook added

--- src/components/UploadForm.js
+++ src/components/UploadForm.js
@@ -16,7 +16,10 @@ function UploadForm() {
   };
   return (
     <form>
-      <input type="file" onChange={changeHandler} />
+      <label>
+        <input type="file" onChange={changeHandler} />
+        <span>+</span>
+      </label>
       <div className="output">
         {error && <div className="error">{error}</div>}
         {file && <div>{file.name}</div>}
